5cc9e843c00f23485e12b42af54641baa4b8122d
Lawrence Bethlenfalvy <lbfalvy@protonmail.com>
2022-06-15 12:14:47 +0100

    Update minimal-booky.js
    It should not be possible for the page to steal focus from the browser's search bar, but if this happens shoot me an e-mail and I'll disable it.

--- dist/minimal-booky.js
+++ dist/minimal-booky.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Minimal Booky
 // @namespace    http://booky.io/
-// @version      1.0
+// @version      2.0
 // @updateURL    https://lbfalvy.github.io/minimal-booky/dist/minimal-booky.js
 // @downloadURL  https://lbfalvy.github.io/minimal-booky/dist/minimal-booky.js
-// @description  Hide the header and footer on Booky.io by default, enable toggling it by clicking on the header
+// @description  Hide the header and footer on Booky.io by default, enable toggling it by clicking on the header and extend search functionality
 // @author       Lawrence Bethlenfalvy <lbfalvy@protonmail.com>
 // @match        https://booky.io/
 // @match        https://booky.io/**
@@ -16,45 +16,84 @@
     'use strict';
     console.log('Compact script loaded');
 
+    // ================================
+    // Globals
+    // ================================
+
+    // === Element selectors ===
+    // DO NOT STORE RESULT
     const header = () => document.querySelector('header.header');
     const footer = () => document.querySelector('footer.footer');
     const headline = () => document.querySelector('h1.headline');
+    const search = () => document.getElementById('search-desktop');
 
-    // Wait for render
-    await Promise.resolve();
-
-    // Construct stylesheet
-    const style = document.createElement('style');
-    document.head.appendChild(style);
-    const sheet = style.sheet;
-    sheet.insertRule(`
-        header.header.closed, footer.footer.closed {
-            display: none;
-        }
-    `);
-    sheet.insertRule(`
-        main.page {
-            padding-bottom: 0rem;
-        }
-    `);
-    sheet.insertRule(`
-        h1.headline:hover {
-            cursor: pointer;
-            color: white;
-        }
-    `);
-
-    // Add evnt listener and class
-    document.body.addEventListener('click', e => {
-        if (e.target !== headline()) return;
-        console.log('Toggling compact mode');
-        header().classList.toggle('closed');
-        footer().classList.toggle('closed');
-    });
-    if (1 < location.pathname.length) return;
-    for (let i = 0; i < 10; i++) { // This is quite possibly the dirtiest hack I've ever written.
+    // === Hooks ===
+    const setupStyles = sheet => { // Stylesheet initialization
+        // General styles
+        sheet.insertRule(`header.header.closed, footer.footer.closed { display: none; }`); // Rather self-explanatory
+        sheet.insertRule(`main.page { padding-bottom: 0rem; }`); // No idea why this was here to begin with
+        // Titlebar styles
+        sheet.insertRule(`
+            h1.headline {
+                width: 264px !important;
+                flex-grow: 0;
+            }
+        `); // The sidebar is 300px and the toolbar's left padding is 36px
+        sheet.insertRule(`
+            h1.headline:hover {
+                cursor: pointer;
+                color: white;
+            }
+        `);
+        sheet.insertRule(`.search-field { flex-grow: 1; }`); // Search form
+        sheet.insertRule(`.search-field__input { width: 100% !important; }`); // Search input
+    }
+    const setupEvents = () => { // Event listeners must not depend on the persistence of selector results
+        document.body.addEventListener('click', e => {
+            if (e.target !== headline()) return;
+            console.log('Toggling compact mode');
+            header().classList.toggle('closed');
+            footer().classList.toggle('closed');
+        });
+        document.body.addEventListener('keydown', e => {
+            if (e.target !== search()) return;
+            console.log('Search input');
+            if (!e.ctrlKey || e.key !== 'Enter') return;
+            console.log('Accepted command');
+            execCommand(search().value);
+        });
+    }
+    const setupRepeat = async () => { // Setup ops that may be repeated to ensure that they're applied early but also override React.
         header()?.classList.add('closed');
         footer()?.classList.add('closed');
         await new Promise(r => setTimeout(r, 100));
+        if (search()) search().placeholder = 'Search bookmarks, or press Enter to navigate or search the web with a / prefix'
     }
+    const setupNorepeat = async () => { // Setup ops that should not be repeated
+        search().focus()
+    }
+    // === Procedures ===
+    const execCommand = cmd => { // Interpret string as command
+        if (cmd[0] == '/') webSearch(cmd.slice(1));
+        else window.location.href = `https://${cmd}`;
+    }
+    const webSearch = q => { // Search for string with DDG
+        let sp = new URLSearchParams({ q });
+        window.location.href = `https://duckduckgo.com/?${sp}`
+    }
+
+    // ================================
+    // Initialization logic
+    // ================================
+    await Promise.resolve(); // Defer
+    // Add styles to header
+    const style = document.createElement('style');
+    document.head.appendChild(style);
+    const sheet = style.sheet;
+    setupStyles(sheet);
+    // Call various setup functions from payload
+    setupEvents();
+    if (1 < location.pathname.length) return; // Ensure none of the following is enacted on non-start pages
+    setTimeout(setupNorepeat, 500); // If the page can't load in half a second, this part will just fail
+    for (let i = 0; i < 10; i++) await setupRepeat(); // This is quite possibly the dirtiest hack I've ever written.
 })();
